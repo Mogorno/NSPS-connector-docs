a8b9b4c1bda3d129e9b05de90fca84c8622ef46a
Oleksandr Luchek <mogorno.97.ly@gmail.com>
2025-09-30 15:17:46 +0300

    add copy llms.txt in /site directory

--- copy_md.py
+++ copy_md.py
@@ -3,6 +3,13 @@ import shutil
 
 DOCS_DIR = "docs"
 SITE_DIR = "site"
+LLMS_FILE = "llms.txt"
+
+if os.path.exists(LLMS_FILE):
+    dest_llms = os.path.join(SITE_DIR, "llms.txt")
+    os.makedirs(os.path.dirname(dest_llms), exist_ok=True)
+    shutil.copy2(LLMS_FILE, dest_llms)
+
 
 for root, _, files in os.walk(DOCS_DIR):
     ''' Copy source .md files to the SITE_DIR so that they are available at the same URL '''
